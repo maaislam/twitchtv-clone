41bdbf3793d389d26b13ce9797c1aae97804c5c7
maaislam <maaislam@outlook.com>
2020-07-13 06:46:53 +0600

    retreiver user id after sign in

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -13,36 +13,57 @@ class GoogleAuth extends Component {
 
 
     componentDidMount(){
+
+        /**
+         * initiate google auth api
+         */
+
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
                 clientId:'134024839870-iulf9vp6a3o9dkpbol7e0vlfpairva2r.apps.googleusercontent.com',
                 scope:'email'
             }).then(()=>{
                 this.auth = window.gapi.auth2.getAuthInstance();
+                /**
+                 * as the component lodas check if user is signed in or not & call appropritate action creator
+                 */
                 this.onAuthChange(this.auth.isSignedIn.get());
+                /**
+                 * keep listening for auth status change
+                 */
                 this.auth.isSignedIn.listen(this.onAuthChange);
             });
         });
         
     };
 
-
+    /**
+     * by default when passed into this.auth.isSignedIn.listen() get a boolean as parameter
+     */
     onAuthChange = (isSignedIn) => {
 
         if (isSignedIn) {
-            this.props.signIn()
+            this.props.signIn(this.auth.currentUser.get().getId())
         }else {
             this.props.signOut();
         }
     };
-    
+    /**
+     * sign in button click handler
+     */
     onSignInClick = () => {
         this.auth.signIn();
     };
-
+    /**
+     * sign out button click handler
+     */
     onSignOutClick = () => {
         this.auth.signOut();
     };
+
+    /**
+     * conditionally renders sign in/out button based on state isSignedIn
+     */
 
     renderAuthButton= () => {
 
